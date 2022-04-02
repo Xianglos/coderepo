52910e874f7d6133b2351d01cf39683fe91d2667
Xianglos <33227442+Xianglos@users.noreply.github.com>
2022-04-03 01:03:03 +0800

    Update dbt.vb
    把取表功能做完了，但是0美观（哇 我居然还知道更新

--- vb/dbt/dbt.vb
+++ vb/dbt/dbt.vb
@@ -17,29 +17,80 @@ Sub query()
         MsgBox ("E1:表不存在")
     End If
     
-    
 End Sub
 '取数据
 Sub selectData(tableName As String)
 '1. 引用ADO工具
 '2. 创建连接对象
-Dim con As New ADODB.Connection
+Dim conn As New ADODB.Connection
 '3. 建立数据库的连接
-con.connectionString = connStr
-con.Open
-MsgBox ("连接成功!" & vbCrLf & "数据库状态：" & con.State & vbCrLf & "数据库版本：" & con.Version)
+'Set rs = New ADODB.Recordset
+conn.Open connStr
+
+MsgBox ("连接成功!" & vbCrLf & "数据库状态：" & conn.State & vbCrLf & "数据库版本：" & conn.Version)
+conn.Close
 ''''''''''''''''''''''''''''''''''''''''
 'TODO
 
+'取得列的个数
+conn.Open connStr
+
+Dim queryCountColsSql As String
+queryCountColsSql = "select count(syscolumns.name) as colname from syscolumns,systypes where syscolumns.xusertype = systypes.xusertype and syscolumns.id = object_id ('" + tableName + "')"
+
+Dim rsCountCols As New ADODB.Recordset
+rsCountCols.Open queryCountColsSql, conn
+
+Dim countCols As Integer
+countCols = Val(rsCountCols.Fields(0).Value)
+
+rsCountCols.Close
+conn.Close
+
+'取得表结构
+conn.Open connStr
+
+Dim querySql As String
+querySql = "select syscolumns.name as colname from syscolumns,systypes where syscolumns.xusertype = systypes.xusertype and syscolumns.id = object_id ('" + tableName + "')"
+
+Dim rsCols As New ADODB.Recordset
+rsCols.Open querySql, conn
+
+'绘制表结构
+Cells(ActiveCell.Row + 1, ActiveCell.Column - 1).Value = "列名"
+
+Dim i As Integer
+For i = 0 To countCols - 1 Step 1
+    
+    Cells(ActiveCell.Row + 1, ActiveCell.Column + i).Value = rsCols(0).Value
+    rsCols.MoveNext
+        
+Next
+
+rsCols.Close
+conn.Close
+
+'取得表数据
+'select row_number()over(order by guid) as colnum,* from URL_LONG_TO_SHROT;
+
+conn.Open connStr
+Dim queryDataSql As String
+queryDataSql = "select row_number()over(order by " + Cells(ActiveCell.Row + 1, ActiveCell.Column).Value + ") as colnum,* from " + tableName + ";"
+
+Dim rsData As New ADODB.Recordset
+Set rsData = conn.Execute(queryDataSql)
+
+Range(Chr(ActiveCell.Column + 63) & CStr(ActiveCell.Row + 2)).CopyFromRecordset rsData
+
+conn.Close
+
 ''''''''''''''''''''''''''''''''''''''''
 '4. 关闭数据库连接
-con.Close
-Set con = Nothing
+Set conn = Nothing
 End Sub
 '检查表是否存在
 Private Function isTableExists(tableName As String) As Boolean
 
-    Dim con As New ADODB.Connection
     Dim conn As ADODB.Connection
     Dim rs As ADODB.Recordset
     Set conn = New ADODB.Connection
@@ -51,7 +102,7 @@ Private Function isTableExists(tableName As String) As Boolean
     conn.connectionString = connStr
     conn.Open
     '拼接sql，查找指定表的列的个数
-    querySql = "SELECT COUNT(*) AS CNT FROM SYSCOLUMNS WHERE ID = OBJECT_ID('" + tableName + "');"
+    querySql = "select count(syscolumns.name) as colname from syscolumns,systypes where syscolumns.xusertype = systypes.xusertype and syscolumns.id = object_id ('" + tableName + "')"
     rs.Open querySql, conn
     countCol = Val(rs.GetString)
     
