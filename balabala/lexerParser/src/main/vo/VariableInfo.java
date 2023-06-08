e1bcfa363ae790b184a26ff76a7b1ea1f167fbc7
Xianglos <33227442+Xianglos@users.noreply.github.com>
2023-06-08 22:41:32 +0800

    Create VariableInfo.java

---
+++ balabala/lexerParser/src/main/vo/VariableInfo.java
@@ -0,0 +1,43 @@
+package main.vo;
+
+public class VariableInfo {
+
+    /** 变量名 */
+    public String name;
+
+    /** 第几次出现 */
+    public int time;
+
+    /** 这次出现时在第几行 */
+    public int linenum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getLinenum() {
+        return linenum;
+    }
+
+    public void setLinenum(int linenum) {
+        this.linenum = linenum;
+    }
+
+    @Override
+    public String toString() {
+        return "VariableInfo [name=" + name + ", time=" + time + ", linenum=" + linenum + "]";
+    }
+
+}
