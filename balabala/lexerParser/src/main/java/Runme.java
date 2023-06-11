ca145cee94255f00625e88b16e2f81b8cd9d7e8b
Xianglos <33227442+Xianglos@users.noreply.github.com>
2023-06-11 13:29:47 +0800

    Update Runme.java

--- balabala/lexerParser/src/main/java/Runme.java
+++ balabala/lexerParser/src/main/java/Runme.java
@@ -10,16 +10,28 @@ public class Runme {
 
     public static void main(String[] args) {
 
+        lexerParserList = new ArrayList<LexerParser>();
+
+        // 递归遍历指定文件夹内所有文件
+        String filePath = "D:\\workspace\\java\\lexerParser\\src\\test\\resources";
+        mainRecursion(filePath);
+
+    }
+
+    /**
+     * 递归遍历指定文件夹内所有文件
+     * 
+     */
+    public static void mainRecursion(String folderPath) {
         long startTime = System.currentTimeMillis();
 
         lexerParserList = new ArrayList<LexerParser>();
 
         // 递归遍历指定文文件夹内所有文件
-        File folder = new File("D:\\workspace\\java\\lexerParser\\src");
+        File folder = new File(folderPath);
         listFilesForFolder(folder);
-        
-        
-        for(LexerParser lx:lexerParserList) {
+
+        for (LexerParser lx : lexerParserList) {
             System.out.println(lx.toString());
         }
 
@@ -29,18 +41,22 @@ public class Runme {
 
     }
 
+    /**
+     * 递归遍历指定文文件夹内所有文件 现在指定后缀名是.java
+     * 
+     */
     public static void listFilesForFolder(final File folder) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesForFolder(fileEntry);
             } else {
-                if (fileEntry.getName().endsWith(".java")) {
+                if (fileEntry.getName().endsWith(".jav")) {
+
+                    System.out.println("\n" + fileEntry.getAbsolutePath());
 
                     // TODO Auto-generated method stub
 
                     LexerParser parser = new LexerParser(fileEntry.getAbsolutePath());
-                    parser.getVariableInfo();
-
                     lexerParserList.add(parser);
 
                 }
