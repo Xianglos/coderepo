3996748f8a681de0eb30263ab238fd3fe6a376d3
Xianglos <33227442+Xianglos@users.noreply.github.com>
2023-07-09 14:31:23 +0800

    Create FuncInfo.java

---
+++ balabala/lexerParser/src/main/vo/FuncInfo.java
@@ -0,0 +1,31 @@
+package main.vo;
+
+public class FuncInfo {
+
+    /** 变量名 */
+    public String name;
+
+    /** 这次出现时在第几行 */
+    public int linenum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLinenum() {
+        return linenum;
+    }
+
+    public void setLinenum(int linenum) {
+        this.linenum = linenum;
+    }
+
+    @Override
+    public String toString() {
+        return "VariableInfo [name=" + name + ", linenum=" + linenum + "]";
+    }
+}
